1af1c623bb19de9ecc4270ee55699c60f16bbc66
Adrian G <adriancortesgonz@gmail.com>
2024-10-26 20:36:57 -0700

    Complete Section: Use Spring Data CrudRespository

--- src/main/kotlin/com/example/demo/MessageService.kt
+++ src/main/kotlin/com/example/demo/MessageService.kt
@@ -1,5 +1,6 @@
 package com.example.demo
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
 import org.springframework.jdbc.core.query
@@ -7,21 +8,10 @@ import java.util.*
 
 
 @Service
-class MessageService(private val db: JdbcTemplate) {
-    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
-        Message(response.getString("id"), response.getString("text"))
-    }
+class MessageService(private val db: MessageRepository) {
+    fun findMessages(): List<Message> = db.findAll().toList()
 
-    fun findMessageById(id: String): Message? = db.query("select * from messages where id = ?", id) { response, _ ->
-        Message(response.getString("id"), response.getString("text"))
-    }.singleOrNull()
+    fun findMessageById(id: String): Message? = db.findByIdOrNull(id)
 
-    fun save(message: Message): Message {
-        val id = message.id ?: UUID.randomUUID().toString()
-        db.update(
-            "insert into messages values ( ?, ? )",
-            id, message.text
-        )
-        return message.copy(id = id)
-    }
+    fun save(message: Message): Message = db.save(message)
 }
